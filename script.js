f7402c9cd5127419a64752116243082fa9920144
Benjamin <168468938+Benjamin-VilaremCornet@users.noreply.github.com>
2026-02-20 16:39:05 +0000

    sidebar + header ish

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const listlinks=document.querySelectorAll(".sidebar-link");
+const listlinks=document.querySelectorAll('.sidebar-link');
+    const sidebar=document.querySelector("sidebar");
 listlinks.forEach(link=>{
     link.addEventListener("click",()=>{
         alert(link.classList[1]);
     })
+})
+const btnsidebar=document.getElementById("sidebar-button");
+const btnsidebarhidden=document.getElementById("sidebar-button-hidden");
+btnsidebar.addEventListener("click",()=>{
+    if (sidebar.classList.contains("hidden")){
+        sidebar.classList.remove("hidden");
+        btnsidebar.style.transform="rotate(0deg)";
+    }else{
+        sidebar.classList.add("hidden");
+        btnsidebar.style.transform="rotate(180deg)";
+    }
 })
